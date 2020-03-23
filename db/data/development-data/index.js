cffa07bd83fd0e94ee1e22a821c49cd99fbab400
WalkerMozes <mozeswalker@outlook.com>
2020-03-23 13:00:50 +0000

    files setup, index's linked, some migrations completed

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const devArticles = require('./articles');
+const devComments = require('./comments');
+const devTopics = require('./topics');
+const devUsers = require('./users');
+
+module.exports = {devArticles, devComments, devTopics, devUsers}
---
+++ db/data/test-data/index.js
@@ -0,0 +1,6 @@
+const testArticles = require('./articles');
+const testComments = require('./comments');
+const testTopics = require('./topics');
+const testUsers = require('./users');
+
+module.exports = {testArticles, testComments, testTopics, testUsers}
